d19eff27e6af0423fc9f106dde6e490ea52d94c0
심승용 <seungyong.shim@mirero.co.kr>
2020-10-16 18:01:37 +0900

    코드

---
+++ consoleapp/AkkaService.cs
@@ -0,0 +1,66 @@
+﻿using Akka.Actor;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace consoleapp
+{
+    internal class AkkaService : IHostedService, IDisposable
+    {
+        private bool isDisposed;
+
+        public AkkaService(ActorSystem actorSystem, ILogger<AkkaService> logger)
+        {
+            ActorSystem = actorSystem;
+            Logger = logger;
+        }
+
+        public ActorSystem ActorSystem { get; }
+        public ILogger<AkkaService> Logger { get; }
+
+        public void Dispose()
+        {
+            // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("StartAsync");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("StopAsync");
+            ActorSystem.Terminate();
+            return CoordinatedShutdown.Get(ActorSystem).Run(CoordinatedShutdown.ClrExitReason.Instance);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    // TODO: 관리형 상태(관리형 개체)를 삭제합니다.
+                }
+
+                // TODO: 비관리형 리소스(비관리형 개체)를 해제하고 종료자를 재정의합니다.
+                // TODO: 큰 필드를 null로 설정합니다.
+                isDisposed = true;
+            }
+        }
+
+        // // TODO: 비관리형 리소스를 해제하는 코드가 'Dispose(bool disposing)'에 포함된 경우에만 종료자를 재정의합니다.
+        // ~AkkaService()
+        // {
+        //     // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+        //     Dispose(disposing: false);
+        // }
+    }
+}
---
+++ consoleapp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHostBuilder hostBuilder = Host
+                .CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<ActorSystem>(sp => Akka.Actor.ActorSystem.Create("Sample"));
+                    services.AddHostedService<AkkaService>();
+                })
+                .UseWindowsService();
+
+            await hostBuilder.Build().RunAsync();
+
+        }
+    }
+}
